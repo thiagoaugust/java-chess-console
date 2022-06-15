431f44ceef6694a13bd29fa811a4a6873e15a4fa
Thiago Rasquino <thiago.programador.net@gmail.com>
2022-06-15 10:12:07 -0300

    Alteração do package e criação da classe posição

---
+++ src/app/App.java
@@ -0,0 +1,12 @@
+package app;
+
+import tabuleiro.Posicao;
+
+public class App {
+
+	public static void main(String[] args) {
+		Posicao posicao = new Posicao(1,2);
+		System.out.println(posicao);
+	}
+
+}
