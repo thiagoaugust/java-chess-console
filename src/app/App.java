bfb8f51ef5fe082e2f8505cede00652ed2db4723
Thiago Rasquino <thiago.programador.net@gmail.com>
2022-06-16 17:14:23 -0300

    Implementação da classe Peça e Tabuleiro

--- src/app/App.java
+++ src/app/App.java
@@ -1,12 +1,13 @@
 package app;
 
-import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class App {
 
 	public static void main(String[] args) {
-		Posicao posicao = new Posicao(1,2);
-		System.out.println(posicao);
+
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+
 	}
 
 }
